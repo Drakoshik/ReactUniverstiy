e91bddf6e56c8554673187dbe6a401a2c3d820c8
h1nlav <hinlav@gmail.com>
2022-12-05 22:45:34 +0200

    tmp

--- lab2-app/src/App.js
+++ lab2-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const counters = [
   { id: 1, initial: 6, min: -5, max: 10 },
@@ -7,34 +7,53 @@ const counters = [
   { id: 3 }
 ]
 
-const value = 0;
+const Counter = ({ counter, onChange }) => {
+  const [count, setCount] = useState(counter.initial || 0)
 
-const Counter = () => {
-  const [count, setCount] = useState(value)
+  useEffect(() => {
+    if (Number.isInteger(count)) {
+      onChange(counter.id, count)
+    }
+  }, [count])
 
   return (
     <div>
       <p>
         Score {count}
-        <button onClick={(e) => { setCount(count + 1) }}>+</button>
-        <button onClick={(e) => { setCount(count - 1) }}>-</button>
-        <button onClick={(e) => { setCount(count = 0) }}>Reset</button>
-
+        <button onClick={() => {
+          if (!Object.keys(counter).includes('max') || (count + 1 <= counter.max)) setCount(count + 1)
+        }}>+</button>
+        <button onClick={() => {
+          if (!Object.keys(counter).includes('min') || (count - 1 >= counter.min)) setCount(count - 1)
+        }}>-</button>
+        <button onClick={() => { setCount(counter.initial || 0) }}>Reset</button>
       </p>
     </div>
   )
 }
 
 const App = () => {
+  const [totalArr, setTotalArr] = useState(counters.map(i => ({ id: i.id, count: i.initial })))
 
   return (
     <>
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
+      {counters.map((c) =>
+        <Counter
+          key={c.id}
+          counter={c}
+          onChange={(id, count) => {
+            const tmp = [...totalArr];
+            const index = tmp.findIndex(i => i.id === id)
+            if (index !== -1) tmp[index].count = count;
+            else tmp.push({ id, count })
+            setTotalArr(tmp)
+          }}
+        />
+      )}
+
+      <div>
+        Total: {totalArr.reduce((p, c) => p += c.count, 0)}
+      </div>
     </>
   );
 }
