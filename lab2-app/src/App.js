632ce2158562f77dad89c7b22d19f850c0f53df7
h1nlav <hinlav@gmail.com>
2022-12-05 22:19:12 +0200

    tmp

--- lab2-app/src/App.js
+++ lab2-app/src/App.js
@@ -2,29 +2,40 @@ import './App.css';
 import { useState } from 'react';
 
 const counters = [
-  {id: 1, initial: 6, min: -5, max: 10},
-  {id: 2, initial: 5},
-  {id: 3}
+  { id: 1, initial: 6, min: -5, max: 10 },
+  { id: 2, initial: 5 },
+  { id: 3 }
 ]
 
-const value = 6;
-const App = () => {
+const value = 0;
 
-  const [count1, setCount1] = useState(6)
-  const [count2, setCount2] = useState(1)
-  const [count3, setCount3] = useState(1)
-  
+const Counter = () => {
+  const [count, setCount] = useState(value)
+
   return (
     <div>
       <p>
-        
-         Score {count1} 
-        <button onClick = {(e) => {setCount1(count1 + 1)}}>+</button>
-        <button onClick = {(e) => {setCount1(count1 - 1)}}>-</button>
-        <button onClick = {(e) => {setCount1(count1 = 0)}}>Reset</button>
+        Score {count}
+        <button onClick={(e) => { setCount(count + 1) }}>+</button>
+        <button onClick={(e) => { setCount(count - 1) }}>-</button>
+        <button onClick={(e) => { setCount(count = 0) }}>Reset</button>
 
       </p>
     </div>
+  )
+}
+
+const App = () => {
+
+  return (
+    <>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+    </>
   );
 }
 
